a490547066d4876670a18bd4a0c6c1bd3cb7646a
amr mustafa <78182930+amrmustafa02@users.noreply.github.com>
2023-11-20 21:32:56 +0200

    Delete test/widget_test.dart

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dio/dio.dart';
-import 'package:route_mobile_task/core/api/api_manager.dart';
-
-Future<void> main() async {
-  ApiManager api = ApiManager();
- var s = await api.getUserData(5746829);
- print(s);
-}
